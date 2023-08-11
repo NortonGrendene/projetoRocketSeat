1b96803e3a5356eb53adbf8dffa3d8ff60b92301
Norton Fernandes Coelho <norton.coelho@grendene.com.br>
2023-08-10 22:15:16 -0300

    :tada: initial commit

---
+++ Html/script.js
@@ -0,0 +1,26 @@
+function toogleMode() {
+  
+    const html = document.documentElement
+
+    if(html.classList.contains('light')){
+
+        html.classList.remove('light')
+    }
+    else {
+        html.classList.add('light') }
+     /* 
+    html.classList.toggle("light")
+    */
+   const img = document.querySelector("#profile img")
+
+   if(html.classList.contains('light')){
+
+    img.setAttribute('src', './img/Avatar.png')
+}
+else {
+    img.setAttribute('src', './img/Avatar2.png') }
+}
+
+
+
+
